16bad213ca90890b5053a2c7da35d758c6c0b8d0
Pasupuleti416 <pasupuletiharitha68@gmail.com>
2024-10-15 20:53:02 +0530

    Update colourcode.h

--- colourcode.h
+++ colourcode.h
@@ -5,5 +5,9 @@ void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
     TelCoColorCoder::MinorColor expectedMinor);
 
+void testPairToNumber(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber);
 
  
